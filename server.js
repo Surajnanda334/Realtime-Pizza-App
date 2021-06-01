4ea8cb23ef04902c805a3afc597cdc0ca6bf43ec
Suraj Nanda <50697016+Surajnanda334@users.noreply.github.com>
2021-06-01 07:33:43 +0530

    Cart page and AUth

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { static } = require('express')
 const express = require('express')
+const expressEjsLayouts = require('express-ejs-layouts')
 const app = express()
 const PORT = process.env.PORT || 3000
 const path = require('path')
@@ -7,12 +8,24 @@ const path = require('path')
 // assets
 app.use(express.static('public'))
 
+app.use(expressEjsLayouts)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-app.use('/', (req,res) =>{
+
+app.get('/', (req,res) =>{
   res.render('home');
 })
+app.get('/cart', (req,res) =>{
+  res.render('customers/cart');
+})
+app.get('/login', (req,res) =>{
+  res.render('auth/login');
+})
+app.get('/register', (req,res) =>{
+  res.render('auth/register');
+})
+
 
 app.listen(PORT , () => {
   console.log(`Listening on port http://localhost:${PORT}`)
