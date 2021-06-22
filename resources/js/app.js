2a6f76abcc4cad8cea38e74bce3545fa802544e7
Suraj Nanda <suraj@antino.io>
2021-06-22 22:23:09 +0530

    add to cart and notifications

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,19 @@
 const axios = require('axios').default;
+import AWN from "awesome-notifications"
+// Set global options
+let options =  {
+  position: 'bottom-right',
+  durations: {
+    global: 1000,
+    success: 1000,
+  }
+}
+// Initialize instance of AWN
+let notifier = new AWN(options)
+
+// Set custom options for next call if needed, it will override globals
+// let nextCallOptions = {...}
+// Call one of available functions
 
 let addToCart = document.querySelectorAll('.add-to-cart')
 let cartCounter = document.querySelector('#cartCounter')
@@ -7,13 +22,13 @@ function updateCart(pizza){
   axios.post('/update-cart', pizza).then(res => {
     console.log(res);
     cartCounter.innerHTML = res.data.totalQty
-    alert(`${pizza.name} added to cart sucessfully`)
+    notifier.success(`${pizza.name} added to cart`, options)
   })
 }
 
 addToCart.forEach((btn) => {
   btn.addEventListener('click', (e)=>{
     let pizza = JSON.parse(btn.dataset.pizza)
-    updateCart(pizza);
+    updateCart(pizza)
   }) 
 })
