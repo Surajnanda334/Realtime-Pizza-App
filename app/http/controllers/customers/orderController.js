4ef4a781df572a7d98a708977ec5ee9e9b84f912
suraj <surajnanda334@gmail.com>
2021-09-13 08:05:39 +0530

    adding order functionality

---
+++ app/http/controllers/customers/orderController.js
@@ -0,0 +1,9 @@
+const store = async (req,res) => {
+    try{
+        console.log(req.body);
+    }catch (error) {
+
+    }
+}
+
+module.exports = { store };
--- routes/web.js
+++ routes/web.js
@@ -1,23 +1,25 @@
-const homeController = require('../app/http/controllers/homeController')
-const authController = require('../app/http/controllers/authController')
-const cartController = require('../app/http/controllers/customers/cartController')
+const homeController = require('../app/http/controllers/homeController');
+const authController = require('../app/http/controllers/authController');
+const cartController = require('../app/http/controllers/customers/cartController');
+const { store } = require('../app/http/controllers/customers/orderController');
 const guest = require('../app/http/middlewares/guest');
 
-function initRoutes(app){
-  
-  app.get('/', homeController().index)
+function initRoutes(app) {
+	app.get('/', homeController().index);
 
-  app.get('/login', guest, authController().login)
-  app.post('/login', authController().postLogin)
-  
-  app.get('/register', guest, authController().register)
-  app.post('/register', authController().postRegister)
-  
-  app.post('/logout', authController().logout)
+	app.get('/login', guest, authController().login);
+	app.post('/login', authController().postLogin);
 
-  app.get('/cart', cartController().index)
+	app.get('/register', guest, authController().register);
+	app.post('/register', authController().postRegister);
 
-  app.post('/update-cart', cartController().update)
+	app.post('/logout', authController().logout);
+
+	app.get('/cart', cartController().index);
+
+	app.post('/update-cart', cartController().update);
+
+  app.post('/orders', store)
 }
 
-module.exports=initRoutes
+module.exports = initRoutes;
